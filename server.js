a2b26a14f63f995005391235b75f63c65530a75c
UCLAShawn3G <uclashawn1@gmail.com>
2019-05-14 21:26:39 -0700

    backend API routes, user model

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// initialize db connection
+connectDB();
+
 app.get("/", (req, res) => res.send("API running"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+// app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 

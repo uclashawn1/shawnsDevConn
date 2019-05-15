14fb529527ef95a4f1c7bb5f9f2eca5730764dc4
UCLAShawn3G <uclashawn1@gmail.com>
2019-05-15 01:46:59 -0700

    posts, user, profile routes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // initialize db connection
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running"));
 
 // Define Routes
